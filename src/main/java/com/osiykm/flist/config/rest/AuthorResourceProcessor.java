dc90b93a5bd39676d46f8b597fce88608f7cac27
osiykm <osiykmukola@gmail.com>
2017-09-19 00:26:52 +0300

    add AuthorProcessor

---
+++ src/main/java/com/osiykm/flist/config/rest/AuthorResourceProcessor.java
@@ -0,0 +1,25 @@
+package com.osiykm.flist.config.rest;
+
+import com.osiykm.flist.entities.Author;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorResourceProcessor implements ResourceProcessor<Resource<Author>> {
+    private final RepositoryEntityLinks links;
+
+    @Autowired
+    public AuthorResourceProcessor(RepositoryEntityLinks links) {
+        this.links = links;
+    }
+
+    @Override
+    public Resource<Author> process(Resource<Author> resource) {
+        resource.add(links.linkToSingleResource(resource.getContent()).withRel("update"));
+        resource.add(links.linkToSingleResource(resource.getContent()).withRel("delete"));
+        return resource;
+    }
+}
