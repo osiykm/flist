8c9f9dc5ab2b0fd68a9db8e68f069d6a5a947fe4
osiykm <osiykmukola@gmail.com>
2017-09-19 17:09:58 +0300

    move fix and add hal-browser

--- src/main/java/com/osiykm/flist/repositories/CategoryRepository.java
+++ src/main/java/com/osiykm/flist/repositories/CategoryRepository.java
@@ -2,9 +2,13 @@ package com.osiykm.flist.repositories;
 
 import com.osiykm.flist.entities.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    @RestResource(exported=false)
+
     Category findByCode(String code);
+    @RestResource(exported=false)
     Boolean existsByName(String name);
 }
