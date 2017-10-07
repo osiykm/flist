fa4ca7938fb5923efaa042b6cdf46fedb240e443
osiykm <osiykmukola@gmail.com>
2017-10-07 19:35:28 +0300

    add CORS filter

---
+++ src/main/java/com/osiykm/flist/entities/BookProj.java
@@ -0,0 +1,30 @@
+package com.osiykm.flist.entities;
+
+import com.osiykm.flist.entities.Author;
+import com.osiykm.flist.entities.Book;
+import com.osiykm.flist.entities.Category;
+import com.osiykm.flist.enums.BookStatus;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.Date;
+import java.util.Set;
+
+/***
+ * @author osiykm
+ * created 05.10.2017 21:17
+ */
+@Projection(name = "fullBook",types = {Book.class})
+public interface BookProj {
+    String getName();
+    String getDescription();
+    String getCommentary();
+    BookStatus getStatus();
+    Author getAuthor();
+    Set<Category> getCategories();
+    String getUrl();
+    Date getAdded();
+    Date getUpdated();
+    Date getCreated();
+    Integer getSize();
+    Integer getChapters();
+}
